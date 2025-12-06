ebf13752d310342e5c77e9c23e103e1106d22e9a
AmanVishwakarma2 <amanvishwakarma872004@gmail.com>
2025-12-06 15:45:47 +0530

    Update storage.js

--- backend/storage.js
+++ backend/storage.js
@@ -14,7 +14,7 @@ try {
 
 
   // MongoDB Connection
-  mongoose.connect(config.connectionString, {
+  mongoose.connect(process.env.MONGODB_URI, {
     serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
     socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
   })
